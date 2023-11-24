09d3a81093e0ca0bbd7dc17c784987b9e9107a5f
Romaniuk Yevhen <greenapteka.com.ua@gmail.com>
2023-11-24 11:44:57 +0100

    end3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,7 +9,7 @@ export const App = () => {
   const [filter, setFilter] = useState('');
   const [contacts, setContacts] = useState (() => {
     return (
-      JSON.parse(window.localStorage.getItem('contakts')) ?? [
+      JSON.parse(window.localStorage.getItem('contacts')) ?? [
         { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
         { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
         { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
